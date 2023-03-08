3f9e50bad363fba20f14d85c92f5bb5b50e04804
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-09 06:35:02 +0700

    feat: add response success and failed

--- utils/response.go
+++ utils/response.go
@@ -3,5 +3,26 @@ package utils
 type Response struct {
 	Status  bool   `json:"status"`
 	Message string `json:"message"`
+	Error   any    `json:"error"`
 	Data    any    `json:"data"`
+}
+
+func BuildResponseSuccess(status bool, message string, data any) Response {
+	res := Response{
+		Status:  status,
+		Message: message,
+		Error:   nil,
+		Data:    data,
+	}
+	return res
+}
+
+func BuildResponseFailed(message string, err string, data any) Response {
+	res := Response{
+		Status: false,
+		Message: message,
+		Error: nil,
+		Data: data,
+	}
+	return res
 }
