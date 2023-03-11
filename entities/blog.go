2317a430b3187e1ab925a99c9edef17e95ad3d26
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-11 15:46:25 +0700

    feat: add entities user and hashpassword

--- entities/blog.go
+++ entities/blog.go
@@ -7,7 +7,7 @@ import (
 type Blog struct {
 	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
 	Blog      string    `gorm:"type:varchar(255)" json:"blog"`
-	Like      uint64    `gorm:"type:uint64" json:"like"`
+	Like      uint64    `json:"like"`
 	Comment   string    `gorm:"type:varchar(255)" json:"comment"`
 	Author    string    `gorm:"type:varchar(255)" json:"author"`
 	UserID    uuid.UUID `gorm:"type:uuid" json:"user_id"`
--- entities/user.go
+++ entities/user.go
@@ -1,17 +1,30 @@
 package entities
 
 import (
+	"github.com/Caknoooo/Golang-BLOG/helpers"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Name      string    `gorm:"type:varchar(100)" json:"name"`
-	Email     string    `gorm:"type:varchar(100);uniqueIndex" json:"email"`
-	Password  string    `gorm:"type:varchar(100);not null" json:"password"`
-	Blogs     []Blog    `gorm:"foreignKey:UserID" json:"blogs,omitempty"`
-	Likes     []Like    `gorm:"foreignKey:UserID" json:"likes,omitempty"`
-	Comments  []Comment `gorm:"foreignKey:UserID" json:"comments,omitempty"`
+	ID       uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	Nama     string    `gorm:"type:varchar(100)" json:"nama"`
+	NoTelp   string    `gorm:"type:varchar(30)" json:"no_telp"`
+	Email    string    `gorm:"type:varchar(100)" json:"email"`
+	Password string    `gorm:"type:varchar(100)" json:"password"`
+	Role     string    `gorm:"type:varchar(100)" json:"role"`
+	Blogs    []Blog    `gorm:"foreignKey:UserID" json:"blogs,omitempty"`
+	Likes    []Like    `gorm:"foreignKey:UserID" json:"likes,omitempty"`
+	Comments []Comment `gorm:"foreignKey:UserID" json:"comments,omitempty"`
 
 	Timestamp
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	var err error
+	u.Password, err = helpers.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+	return nil
 }
