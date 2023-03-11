3af9209c4f12c664fbdb8b9a09602fae257115a1
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-11 15:44:31 +0700

    feat: add endpoint for api

--- routes/router.go
+++ routes/router.go
@@ -1,7 +1,20 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Caknoooo/Golang-BLOG/controller"
+	"github.com/Caknoooo/Golang-BLOG/middleware"
+	"github.com/Caknoooo/Golang-BLOG/services"
+	"github.com/gin-gonic/gin"
+)
 
-func Router() *gin.Engine{
-	return gin.Default()
+func Router(route *gin.Engine, UserController controller.UserController, jwtService services.JWTService){
+		routes := route.Group("/api/user")
+		{
+			routes.POST("", UserController.RegisterUser)
+			routes.GET("", middleware.Authenticate(jwtService), UserController.GetAllUser)
+			routes.POST("/login", UserController.LoginUser)
+			routes.DELETE("/", middleware.Authenticate(jwtService), UserController.DeleteUser)
+			routes.PUT("/", middleware.Authenticate(jwtService), UserController.UpdateUser)
+			routes.GET("/me", middleware.Authenticate(jwtService), UserController.MeUser)
+		}
 }
